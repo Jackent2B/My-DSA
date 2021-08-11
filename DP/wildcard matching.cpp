20ce314cb2a248a86268d03a54d4db997796a7ff
Jayant Khandelwal <jayant.khandelwal7@gmail.com>
2021-08-11 16:06:56 +0530

    wildcard matching updated

--- DP/wildcard matching.cpp
+++ DP/wildcard matching.cpp
@@ -1,3 +1,41 @@
+
+// class Solution {
+// public:
+    
+//         #define ll long long int
+
+// bool go(string &str,string &pat,unordered_map<string,bool> &mp
+// ,ll s=0,ll p=0)
+// {
+//  string temp=to_string(s)+"|"+to_string(p);
+//  if(mp.count(temp))
+//   return mp[temp];
+//  if(p==pat.length())
+//  {
+//   return mp[temp]=(s==str.length());
+//  }
+//  if(s==str.length())
+//  {
+//   for(ll i=p;i<pat.length();i++)
+//    if(pat[i]!='*')
+//     return (mp[temp]=false);
+//   return (mp[temp]=true);
+//  }
+
+//  if(pat[p]=='?' or pat[p]==str[s])
+//   return mp[temp]=go(str,pat,mp,s+1,p+1);
+//  if(pat[p]=='*')
+//   return mp[temp]=go(str,pat,mp,s+1,p) or go(str,pat,mp,s,p+1);
+//  return mp[temp]=false;
+// }
+    
+//     bool isMatch(string s, string p) {
+//                 unordered_map<string,bool> mp;
+
+//         return go(s,p,mp);
+//     }
+// };
+
 class Solution {
 public:
     int dp[20001][20001];
